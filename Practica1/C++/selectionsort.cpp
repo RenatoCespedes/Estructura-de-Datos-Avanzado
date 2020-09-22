eda9be0a5fa0c66b34bb95b3756339548e17f966
shirlwin22 <shiirley597@gmail.com>
2020-09-22 14:30:51 -0500

    Selection sort con tiempo

--- Practica1/C++/selectionsort.cpp
+++ Practica1/C++/selectionsort.cpp
@@ -34,9 +34,16 @@ void print(int arr[], int tam)
   
 int main()  
 {  
+    unsigned t0,t1;
+    srand(time(NULL));
+
     int arr[] = {64,10,1,9,85,36,14,3,6, 25, 12, 22, 11};  
-    int n = sizeof(arr)/sizeof(arr[0]);  
-    selectionSort(arr, n);   
+    int n = sizeof(arr)/sizeof(arr[0]);
+    t0=clock();
+    selectionSort(arr, n); 
+    t1=clock();
     print(arr, n);  
+    double time=(double(t1-t0)/CLOCKS_PER_SEC);
+    cout<<"Tiempo 1 "<<time<<endl;
     return 0;  
 } 
