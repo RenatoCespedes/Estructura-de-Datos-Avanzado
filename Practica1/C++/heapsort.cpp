0cd2b892722acb4b36776e57c4fbdcf1a3bb393c
unknown <rmamanisuc@unsa.edu.pe>
2020-09-23 22:30:01 -0500

    Heapsort

--- Practica1/C++/heapsort.cpp
+++ Practica1/C++/heapsort.cpp
@@ -1,35 +1,33 @@
 // C++ program for implementation of Heap Sort
 #include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-void heapify(int arr[], int n, int i)
+void max_heap(long long arr[], long long n, int i)
 {
-	int largest = i; // Initialize largest as root
-	int l = 2*i + 1; // left = 2*i + 1
-	int r = 2*i + 2; // right = 2*i + 2
-	if (l < n && arr[l] > arr[largest])
-		largest = l;
-	if (r < n && arr[r] > arr[largest])
-		largest = r;
-	if (largest != i)
+	int mayor = i;
+	int l = 2*i + 1;
+	int r = 2*i + 2;
+	if (l < n && arr[l] > arr[mayor])
+		mayor = l;
+	if (r < n && arr[r] > arr[mayor])
+		mayor = r;
+	if (mayor != i)
 	{
-		swap(arr[i], arr[largest]);
-		heapify(arr, n, largest);
+		swap(arr[i], arr[mayor]);
+		max_heap(arr, n, mayor);
 	}
 }
-
-// main function to do heap sort
-void heapSort(int arr[], int n)
+void heapSort(long long arr[], long long n)
 {
 	for (int i = n / 2 - 1; i >= 0; i--)
-		heapify(arr, n, i);
+		max_heap(arr, n, i);
 	for (int i=n-1; i>0; i--)
 	{
 		swap(arr[0], arr[i]);
-		heapify(arr, i, 0);
+		max_heap(arr, i, 0);
 	}
 }
-void printArray(int arr[], int n)
+void printArray(long long arr[], long long n)
 {
 	for (int i=0; i<n; ++i)
 		cout << arr[i] << " ";
@@ -37,12 +35,26 @@ void printArray(int arr[], int n)
 }
 int main()
 {
-	int arr[] = {12, 11, 13, 5, 6, 7};
-	int n = sizeof(arr)/sizeof(arr[0]);
-    cout << "lista desordenada"<<endl;
-    printArray(arr, n);
-	heapSort(arr, n);
+    ofstream archivo("time.txt");
 
-	cout << "lista construida y ordenada: " <<endl;
-	printArray(arr, n);
+	long long arr[100000];
+	srand(time(NULL));
+	long long  num;
+	float t0;
+	double time1;
+	for(long long i=0;i<100000;i++)
+    {
+        num=1+rand()%(10000-1);
+        arr[i]=num;
+    }
+	long long n = sizeof(arr)/sizeof(arr[0]);
+    cout << "lista desordenada"<<endl;
+    t0=clock();
+	heapSort(arr, n);
+	t0=clock()-t0;
+	time1= ((double)t0)/CLOCKS_PER_SEC;
+    printf (" m_Sec :%d (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
+    archivo<<t<<endl;
+	/*cout << "lista construida y ordenada: " <<endl;
+	printArray(arr, n);*/
 }
