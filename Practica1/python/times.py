2285b3344e7839cd048315e8408db2e4b2c27fa1
JosnickCB <42551058+JosnickCB@users.noreply.github.com>
2020-09-24 00:40:59 -0500

    Medidor v0.1

--- Practica1/python/times.py
+++ Practica1/python/times.py
@@ -8,7 +8,7 @@ from mergesort import *
 from quicksort import *
 from selectionsort import *
 for i in range(10000,100001,10000):
-    file = open('Array_'+str(i),'r')
+    file = open('Array_'+str(i)+'.txt','r')
     aux = file.readlines()
     file.close()
     arr = [int(i) for i in aux[0].split()]
@@ -17,54 +17,61 @@ for i in range(10000,100001,10000):
     start = time()
     BubbleSort(a)
     elapsed = time() - start
-    file.write("Length"+str(i)+'\n')
+    file.write("Length "+str(i)+'\n')
     file.write(str(elapsed)+'\n')
     file.close()
+    print('Bubble '+str(i)+' f')
     a = arr
     file = open("python_insertion.txt",'a')
     start = time()
     InsertionSort(a)
     elapsed = time() - start
-    file.write("Length"+str(i)+'\n')
+    file.write("Length "+str(i)+'\n')
     file.write(str(elapsed)+'\n')
     file.close()
+    print('Insert '+str(i)+' f')
     a = arr
     file = open("python_merge.txt",'a')
     start = time()
     mergeSort(a)
     elapsed = time() - start
-    file.write("Length"+str(i)+'\n')
+    file.write("Length "+str(i)+'\n')
     file.write(str(elapsed)+'\n')
     file.close()
+    print('Merge '+str(i)+' f')
     a = arr
     file = open("python_quick.txt",'a')
     start = time()
     quickSort(a,0,i-1)
     elapsed = time() - start
-    file.write("Length"+str(i)+'\n')
+    file.write("Length "+str(i)+'\n')
     file.write(str(elapsed)+'\n')
     file.close()
+    print('Quick '+str(i)+' f')
     a = arr
     file = open("python_selection.txt",'a')
     start = time()
     selectionsort(a)
     elapsed = time() - start
-    file.write("Length"+str(i)+'\n')
+    file.write("Length "+str(i)+'\n')
     file.write(str(elapsed)+'\n')
     file.close()
+    print('Selection '+str(i)+' f')
     a = arr
     file = open("python_counting.txt",'a')
     start = time()
     countingSort(a)
     elapsed = time() - start
-    file.write("Length"+str(i)+'\n')
+    file.write("Length "+str(i)+'\n')
     file.write(str(elapsed)+'\n')
     file.close()
+    print('Counting '+str(i)+' f')
     a = arr
     file = open("python_heap.txt",'a')
     start = time()
     heapSort(a)
     elapsed = time() - start
-    file.write("Length"+str(i)+'\n')
+    file.write("Length "+str(i)+'\n')
     file.write(str(elapsed)+'\n')
     file.close()
+    print('Heap '+str(i)+' f')
