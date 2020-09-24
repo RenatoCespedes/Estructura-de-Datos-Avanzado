c7098f849c0cdee9be9a60e2f85c103c6b51c0bb
RenatoCespedes <71398760+RenatoCespedes@users.noreply.github.com>
2020-09-23 23:00:28 -0500

    Actualizado

--- Practica1/GeneradorAleatorio/generador.cpp
+++ Practica1/GeneradorAleatorio/generador.cpp
@@ -9,7 +9,7 @@ using namespace std;
  int main(){
  	int n=10000,k=0;
  	ofstream fs("Tama.txt");
- 	for(int i=n;i<600000;i+=60000){
+ 	for(int i=n;i<=100000;i+=10000){
  		GenerarArray(i,k);
  		fs<<i<<" ";
  		k=k+1;
